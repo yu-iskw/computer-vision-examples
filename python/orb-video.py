5c28b2db843d303b9fd029ecd04c51dd548c09e7
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2019-03-02 18:06:48 -0800

    Implement orb-video.py

--- python/orb-video.py
+++ python/orb-video.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np
+import copy
 
 cap = cv2.VideoCapture(0)
 
@@ -12,13 +12,24 @@ try:
         ret, frame = cap.read()
 
         # Operate something on the frame come here.
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Merge the images half-and-half
-        frame[:, 0:(frame.shape[1] // 2), :] = hsv[:, 0:(frame.shape[1] // 2), :]
+        # Extract the key points with ORB.
+        orb = cv2.ORB_create(200, 2.0)
+        keypoints, descriptor = orb.detectAndCompute(frame_gray, None)
+
+        # Show the frame with key points.
+        keyp_without_size = copy.copy(frame)
+        keyp_with_size = copy.copy(frame)
+        # Draw the keypoints without size or orientation on one copy of the training image
+        cv2.drawKeypoints(frame, keypoints, keyp_without_size, color=(0, 255, 0))
+        # Draw the keypoints with size and orientation on the other copy of the training image
+        cv2.drawKeypoints(frame, keypoints, keyp_with_size,
+                          flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
         # Display the converted frame.
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame', keyp_without_size)
+        # cv2.imshow('frame', keyp_with_size)
 
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break
